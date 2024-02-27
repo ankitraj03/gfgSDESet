5ce63d9c1930efb10353457981b3d3050d5e3dae
Ankit Raj Sinha <97229534+ankitraj03@users.noreply.github.com>
2024-02-27 11:35:08 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Minimize the sum of product/minimize-the-sum-of-product.cpp
@@ -0,0 +1,44 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution{
+    public:
+    long long int minValue(int a[], int b[], int n)
+    {
+        // Your code goes here
+        sort(a,a+n);
+        sort(b,b+n);
+        long long sum=0;
+        int j=n-1;
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+a[i]*b[j];
+            j--;
+        }
+        return sum;
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+ {
+     int t;
+     cin>>t;
+     while(t--)
+     {
+         int n, i;
+         cin>>n;
+         int a[n], b[n];
+         for(i=0;i<n;i++)
+         cin>>a[i];
+         for(i=0;i<n;i++)
+         cin>>b[i];
+         Solution ob;
+         cout<< ob.minValue(a, b, n) <<endl;
+     }
+	
+	return 0;
+}
+// } Driver Code Ends
